7fedfdd6e6551e4271a48224f859760dd563488a
PauFusco <99730051+PauFusco@users.noreply.github.com>
2022-03-09 18:18:09 +0100

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -114,6 +114,7 @@ bool Game::Update()
 	if (keys[SDL_SCANCODE_DOWN] == KEY_REPEAT)	fy = 1;
 	//if (keys[SDL_SCANCODE_LEFT] == KEY_REPEAT)	fx = -1;
 	//if (keys[SDL_SCANCODE_RIGHT] == KEY_REPEAT)	fx = 1;
+	
 	if (keys[SDL_SCANCODE_SPACE] == KEY_DOWN)
 	{
 		int x, y, w, h;
